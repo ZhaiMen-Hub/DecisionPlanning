0914df7bf73497d2ae30b225a59a27b329ae7460
JC <junchanghuang@outlook.com>
2024-08-23 15:45:33 +0800

    combind 2 Leader Follower game in one cmpc prototype

--- Game-Theoretic Motion Planning/CMPCGame.py
+++ Game-Theoretic Motion Planning/CMPCGame.py
@@ -16,9 +16,10 @@ Q = np.diag([0.0, 1.0, 2.0, 1.0, 2.0, 4.0])
 R = np.diag([4.0, 4.0])
 xF0 = np.array([2.0, 15.0, 0.0, 5.0, 0.0, 0.0])
 xFref = np.array([0.0, 15.0, 0.0, 5.0, 0.0, 0.0])
-# xL0 = np.array([12.0, 10.0, 0.0, 3.0, 0.0, 0.0])
-xL0 = np.array([32.0, 10.0, 0.0, 3.0, 0.0, 0.0])
-xLref = np.array([0.0, 10.0, 0.0, 5.0, 0.0, 0.0])
+# xL0 = np.array([32.0, 10.0, 0.0, 3.0, 0.0, 0.0])
+# xLref = np.array([0.0, 10.0, 0.0, 5.0, 0.0, 0.0])
+xL0 = np.array([32.0, 5.0, 0.0, 3.0, 0.0, 0.0])
+xLref = np.array([0.0, 5.0, 0.0, 5.0, 0.0, 0.0])
 addCollisionCons = True
 vxRef = 10
 KF = 0.01
@@ -61,105 +62,127 @@ B = ca.MX(B_np)
 def dynamics(x, u):
     return ca.mtimes(A, x) + ca.mtimes(B, u)
 
-# Function to define decision variables
-def define_decision_variables(nx, nu, N):
-    XF = ca.MX.sym('XF', nx, N+1)
-    UF = ca.MX.sym('UF', nu, N)
-    XL = ca.MX.sym('XL', nx, N+1)
-    UL = ca.MX.sym('UL', nu, N)
-    return XF, UF, XL, UL
-
-# Function to define the cost function
-def define_cost_function(XF, UF, XL, UL, N, Q, R, xFref, xLref):
-    JF = 0
-    JL = 0
-    for k in range(N):
-        xFrefCa = ca.MX(xFref)
-        xLrefCa = ca.MX(xLref)
-        JF += ca.mtimes([(XF[:, k+1] - xFrefCa).T, Q, (XF[:, k+1] - xFrefCa)]) + ca.mtimes([UF[:, k].T, R, UF[:, k]])
-        JL += ca.mtimes([(XL[:, k+1] - xLrefCa).T, Q, (XL[:, k+1] - xLrefCa)]) + ca.mtimes([UL[:, k].T, R, UL[:, k]])
-    return JF, JL
-
-# # # Function for Leader Follower Game
-def gameLeaderFollower(xL0, xF0, probDecel0, probAccel0):
+# Function for Leader Follower Game with Deceleration and Acceleration
+def gameLeaderFollower(xL0, xF0, probDecel, probAccel):
 
     # 定义优化问题
     nx = 6  # 状态维度
     nu = 2  # 输入维度
     lenX = nx * (N+1)
     lenU = nu * N
-    XF, UF, XL, UL = define_decision_variables(nx, nu, N)
 
-    JF = 0
-    JL = 0
-    # Jinfluence = 0
-    consF = []
-    consL = []
-    collisionConsF = []
-    collisionConsL = []
+    XDecelF = ca.MX.sym('XDecelF', nx, N+1)
+    UDecelF = ca.MX.sym('UDecelF', nu, N)
+    XDecelL = ca.MX.sym('XDecelL', nx, N+1)
+    UDecelL = ca.MX.sym('UDecelL', nu, N)
+    XAccelF = ca.MX.sym('XAccelF', nx, N+1)
+    UAccelF = ca.MX.sym('UAccelF', nu, N)
+    XAccelL = ca.MX.sym('XAccelL', nx, N+1)
+    UAccelL = ca.MX.sym('UAccelL', nu, N)
 
-    # 初始状态
-    consF.append(XF[:, 0] - xF0)
-    consL.append(XL[:, 0] - xL0)
+    # Initialize costs and constraints
+    JDecelF = 0
+    JDecelL = 0
+    JAccelF = 0
+    JAccelL = 0
+    consDecelF = []
+    consDecelL = []
+    consAccelF = []
+    consAccelL = []
+    collisionConsDecelF = []
+    collisionConsDecelL = []
+    collisionConsAccelF = []
+    collisionConsAccelL = []
 
-    # 轨迹规划的目标
+    # Initial state constraints
+    consDecelF.append(XDecelF[:, 0] - xF0)
+    consDecelL.append(XDecelL[:, 0] - xL0)
+    consAccelF.append(XAccelF[:, 0] - xF0)
+    consAccelL.append(XAccelL[:, 0] - xL0)
+
+    # Define reference trajectories
     xFrefCa = ca.MX(xFref)
     xLrefCa = ca.MX(xLref)
+
     for k in range(N):
-        # cost
-        JF += ca.mtimes([(XF[:, k+1] - xFrefCa).T, Q, (XF[:, k+1] - xFrefCa)]) + ca.mtimes([UF[:, k].T, R, UF[:, k]])
-        JL += ca.mtimes([(XL[:, k+1] - xLrefCa).T, Q, (XL[:, k+1] - xLrefCa)]) + ca.mtimes([UL[:, k].T, R, UL[:, k]])
-        # Jinfluence += (XF[1, k+1] - vxRef) ** 2
-        # dynamics constrain
-        xF_next = dynamics(XF[:, k], UF[:, k])
-        xL_next = dynamics(XL[:, k], UL[:, k])
-        consF.append(XF[:, k+1] - xF_next)
-        consL.append(XL[:, k+1] - xL_next)
-        # collision
+        # Cost function for deceleration and acceleration
+        JDecelF += ca.mtimes([(XDecelF[:, k+1] - xFrefCa).T, Q, (XDecelF[:, k+1] - xFrefCa)]) + ca.mtimes([UDecelF[:, k].T, R, UDecelF[:, k]])
+        JDecelL += ca.mtimes([(XDecelL[:, k+1] - xLrefCa).T, Q, (XDecelL[:, k+1] - xLrefCa)]) + ca.mtimes([UDecelL[:, k].T, R, UDecelL[:, k]])
+        JAccelF += ca.mtimes([(XAccelF[:, k+1] - xFrefCa).T, Q, (XAccelF[:, k+1] - xFrefCa)]) + ca.mtimes([UAccelF[:, k].T, R, UAccelF[:, k]])
+        JAccelL += ca.mtimes([(XAccelL[:, k+1] - xLrefCa).T, Q, (XAccelL[:, k+1] - xLrefCa)]) + ca.mtimes([UAccelL[:, k].T, R, UAccelL[:, k]])
+
+        # Dynamics constraints
+        xF_next_dec = dynamics(XDecelF[:, k], UDecelF[:, k])
+        xL_next_dec = dynamics(XDecelL[:, k], UDecelL[:, k])
+        xF_next_acc = dynamics(XAccelF[:, k], UAccelF[:, k])
+        xL_next_acc = dynamics(XAccelL[:, k], UAccelL[:, k])
+
+        consDecelF.append(XDecelF[:, k+1] - xF_next_dec)
+        consDecelL.append(XDecelL[:, k+1] - xL_next_dec)
+        consAccelF.append(XAccelF[:, k+1] - xF_next_acc)
+        consAccelL.append(XAccelL[:, k+1] - xL_next_acc)
+
+        # Collision constraints
         if addCollisionCons:
-            # px_L - px_F > dist  => dist + px_F - px_L < 0
-            collisionConsF.append(distF + XF[0, k+1] - XL[0, k+1])
-            collisionConsL.append(distL + XF[0, k+1] - XL[0, k+1])
+            collisionConsDecelF.append(distDecelF + XDecelF[0, k+1] - XDecelL[0, k+1])
+            collisionConsDecelL.append(distL + XDecelF[0, k+1] - XDecelL[0, k+1])
+            collisionConsAccelF.append(distAccelF + XAccelF[0, k+1] - XAccelL[0, k+1])
+            collisionConsAccelL.append(distL + XAccelF[0, k+1] - XAccelL[0, k+1])
 
-    equConF = ca.vertcat(*consF)
-    equConL = ca.vertcat(*consL)
-    inequConsF = ca.vertcat(*collisionConsF)
-    inequConsL = ca.vertcat(*collisionConsL)
-    equCon = ca.vertcat(equConF, equConL)
-    inequCon = ca.veccat(inequConsF, inequConsL)
+    # Combine constraints
+    equConDecelF = ca.vertcat(*consDecelF)
+    equConDecelL = ca.vertcat(*consDecelL)
+    equConAccelF = ca.vertcat(*consAccelF)
+    equConAccelL = ca.vertcat(*consAccelL)
+    inequConsDecelF = ca.vertcat(*collisionConsDecelF)
+    inequConsDecelL = ca.vertcat(*collisionConsDecelL)
+    inequConsAccelF = ca.vertcat(*collisionConsAccelF)
+    inequConsAccelL = ca.vertcat(*collisionConsAccelL)
+    inequCon = ca.veccat(inequConsDecelF, inequConsDecelL, inequConsAccelF, inequConsAccelL)
 
-    # define Lagrangian multipliers
-    lambda_ = ca.MX.sym('lambda', equConF.size1())
-    mu = ca.MX.sym('nu', inequConsF.size1())
+    # Define Lagrangian multipliers
+    lambda_dec = ca.MX.sym('lambda_dec', equConDecelF.size1())
+    lambda_acc = ca.MX.sym('lambda_acc', equConAccelF.size1())
+    mu_dec = ca.MX.sym('mu_dec', inequConsDecelF.size1())
+    mu_acc = ca.MX.sym('mu_acc', inequConsAccelF.size1())
 
-    # Lagrangian
-    Lagrangian = JF + ca.mtimes([lambda_.T, equConF]) + ca.mtimes([mu.T, inequConsF])
+    # Lagrangian for deceleration and acceleration
+    LagrangianDecel = JDecelF + \
+                      ca.mtimes([lambda_dec.T, equConDecelF]) + \
+                      ca.mtimes([mu_dec.T, inequConsDecelF])
+    LagrangianAccel = JAccelF + \
+                      ca.mtimes([lambda_acc.T, equConAccelF]) + \
+                      ca.mtimes([mu_acc.T, inequConsAccelF])
 
-    # KKT condition (equality)
-    grad_L_x = ca.gradient(Lagrangian, ca.vertcat(ca.reshape(XF, -1, 1), ca.reshape(UF, -1, 1)))
-    complementary_slackness = ca.diag(mu) @ inequConsF
-    equCon = ca.vertcat(equConF, equConL, grad_L_x, complementary_slackness)
+    # KKT conditions
+    grad_L_x_decel = ca.gradient(LagrangianDecel, ca.vertcat(ca.reshape(XDecelF, -1, 1), ca.reshape(UDecelF, -1, 1)))
+    grad_L_x_accel = ca.gradient(LagrangianAccel, ca.vertcat(ca.reshape(XAccelF, -1, 1), ca.reshape(UAccelF, -1, 1)))
+    complementary_slackness_decel = ca.diag(mu_dec) @ inequConsDecelF
+    complementary_slackness_accel = ca.diag(mu_acc) @ inequConsAccelF
 
-    # construct the optimization problem
-    x = ca.vertcat(ca.reshape(XF, -1, 1), ca.reshape(UF, -1, 1), ca.reshape(XL, -1, 1), ca.reshape(UL, -1, 1), lambda_, mu)
+    equCon = ca.vertcat(equConDecelF, equConAccelF, equConDecelL, equConAccelL,
+                        grad_L_x_decel, grad_L_x_accel, complementary_slackness_decel, complementary_slackness_accel)
 
-    # lower bound to x (add constraint mu >= 0)
+    # Construct the optimization problem
+    x = ca.vertcat(ca.reshape(XDecelF, -1, 1), ca.reshape(UDecelF, -1, 1),
+                   ca.reshape(XDecelL, -1, 1), ca.reshape(UDecelL, -1, 1),
+                   ca.reshape(XAccelF, -1, 1), ca.reshape(UAccelF, -1, 1),
+                   ca.reshape(XAccelL, -1, 1), ca.reshape(UAccelL, -1, 1), lambda_dec, lambda_acc, mu_dec, mu_acc)
+
+    # Lower bound to x (add constraint mu >= 0)
     lbx = ca.vertcat(
-        ca.repmat(-ca.inf, 2 * (lenU + lenX) + lambda_.size1(), 1),  # 生成全为 -ca.inf 的列向量
-        ca.repmat(0, mu.size1(), 1)                                  # 生成全为 0 的列向量(for mu)
+        ca.repmat(-ca.inf, 4 * (lenU + lenX) + lambda_dec.size1() + lambda_acc.size1(), 1),  # 生成全为 -ca.inf 的列向量
+        ca.repmat(0, mu_dec.size1() + mu_acc.size1(), 1)                                  # 生成全为 0 的列向量(for mu)
     )
 
-    # nlp = {'x': x, 'f': KF * JF + KL * JL + Kinfluence * Jinfluence, 'g': ca.vertcat(equCon, inequCon)}
-    nlp = {'x': x, 'f': KF * JF + KL * JL, 'g': ca.vertcat(equCon, inequCon)}
+    # NLP formulation
+    nlp = {'x': x, 'f': probDecel * (KF * JDecelF + KL * JDecelL) + probAccel * (KF * JAccelF + KL * JAccelL) , 'g': ca.vertcat(equCon, inequCon)}
 
-    # 设置求解器
+    # Solver options
     opts = {'ipopt': {'print_level': 0}}
     solver = ca.nlpsol('solver', 'ipopt', nlp, opts)
-    # solver = ca.nlpsol('solver', 'ipopt', nlp)
 
-    # 求解优化问题
-    # without initial guess
-    # sol = solver(lbg=ca.DM(equCon.size1() * [0] + inequCon.size1() * [-ca.inf]), ubg=ca.DM((equCon.size1() + inequCon.size1()) * [0]), lbx = lbx)
+    # Solve the optimization problem
     sol = solver(
         lbg=ca.vertcat(
             ca.repmat(0, equCon.size1(), 1),          # 全为 0 的列向量(for equality)
@@ -169,19 +192,23 @@ def gameLeaderFollower(xL0, xF0, probDecel0, probAccel0):
         lbx=lbx
     )
 
-
-    # 提取解
+    # Extract solutions
     sol_x = sol['x'].full().flatten()
-    XF_sol = sol_x[:lenX].reshape(N+1, nx).T
-    UF_sol = sol_x[lenX:lenX+lenU].reshape(N, nu).T
-    XL_sol = sol_x[lenX+lenU:2*lenX+lenU].reshape(N+1, nx).T
-    UL_sol = sol_x[2*lenX+lenU:2*lenX+2*lenU].reshape(N, nu).T
+    XDecelF_sol = sol_x[:lenX].reshape(N+1, nx).T
+    UDecelF_sol = sol_x[lenX:lenX+lenU].reshape(N, nu).T
+    XDecelL_sol = sol_x[lenX+lenU:2*lenX+lenU].reshape(N+1, nx).T
+    UDecelL_sol = sol_x[2*lenX+lenU:2*lenX+2*lenU].reshape(N, nu).T
+    XAccelF_sol = sol_x[2*lenX+2*lenU:3*lenX+2*lenU].reshape(N+1, nx).T
+    UAccelF_sol = sol_x[3*lenX+2*lenU:3*lenX+3*lenU].reshape(N, nu).T
+    XAccelL_sol = sol_x[3*lenX+3*lenU:4*lenX+3*lenU].reshape(N+1, nx).T
+    UAccelL_sol = sol_x[4*lenX+3*lenU:4*lenX+4*lenU].reshape(N, nu).T
 
-    return XF_sol, UF_sol, XL_sol, UL_sol
+    return XDecelF_sol, UDecelF_sol, XDecelL_sol, UDecelL_sol, XAccelF_sol, UAccelF_sol, XAccelL_sol, UAccelL_sol
 
-# XF_sol, UF_sol, XL_sol, UL_sol, _, _, _, _ = gameLeaderFollower(xL0, xF0, 1, 0)
-# XDecelF_sol, UDecelF_sol, XDecelL_sol, UDecelL_sol, XAccelF_sol, UAccelF_sol, XAccelL_sol, UDecelL_sol = gameLeaderFollower(xL0, xF0)
-XF_sol, UF_sol, XL_sol, UL_sol = gameLeaderFollower(xL0, xF0, 1, 0)
+
+XF_sol, UF_sol, XL_sol, UL_sol, _, _, _, _ = gameLeaderFollower(xL0, xF0, probDecel0, probAccel0)
+XDecelF_sol, UDecelF_sol, XDecelL_sol, UDecelL_sol, XAccelF_sol, UAccelF_sol, XAccelL_sol, UDecelL_sol = gameLeaderFollower(xL0, xF0, probDecel0, probAccel0)
+# XF_sol, UF_sol, XL_sol, UL_sol = gameLeaderFollower(xL0, xF0, 1, 0)
 
 np.set_printoptions(precision=2)
 # print('XF_sol')
@@ -213,22 +240,22 @@ plt.legend()
 
 # # vx 轨迹
 # plt.subplot(4, 2, 3)
-# plt.plot(time, X_sol[1, :], 'r-', label='vx')
+# plt.plot(time, XF_sol[1, :], 'r-', label='vx')
 # plt.legend()
 
 # # vy 轨迹
 # plt.subplot(4, 2, 4)
-# plt.plot(time, X_sol[4, :], 'r-', label='vy')
+# plt.plot(time, XF_sol[4, :], 'r-', label='vy')
 # plt.legend()
 
 # # ax 轨迹
 # plt.subplot(4, 2, 5)
-# plt.plot(time, X_sol[2, :], 'r-', label='ax')
+# plt.plot(time, XF_sol[2, :], 'r-', label='ax')
 # plt.legend()
 
 # # ay 轨迹
 # plt.subplot(4, 2, 6)
-# plt.plot(time, X_sol[5, :], 'r-', label='ay')
+# plt.plot(time, XF_sol[5, :], 'r-', label='ay')
 # plt.legend()
 
 # # jx 轨迹
@@ -260,17 +287,23 @@ plt.tight_layout()
 # plt.show()
 
 # Create the animation
-fig, ax = plt.subplots(2, 1, figsize=(10, 8))
+fig, ax = plt.subplots(3, 1, figsize=(10, 8))
+plt.tight_layout()
 
 def update(frame):
 
     # clear plotting of last timestep
     ax[0].clear()
     ax[1].clear()
+    ax[2].clear()
     
     # plot traj
     ax[0].plot(XF_sol[0, : frame+1], XF_sol[3, : frame+1], 'bo-', label='Traj_F')
     ax[0].plot(XL_sol[0, : frame+1], XL_sol[3, : frame+1], 'ro-', label='Traj_L')
+
+    # plot plan
+    ax[0].plot(XDecelL_sol[0, frame+1:], XDecelL_sol[3, frame+1:], 'm+', label='dec')
+    ax[0].plot(XAccelL_sol[0, frame+1:], XAccelL_sol[3, frame+1:], 'mx', label='acc')
 
     # plot collision distance
     ax[0].axvline(x = XF_sol[0, frame] + distF, color='b', linestyle='--', label='collisionF')
@@ -279,13 +312,13 @@ def update(frame):
     # Add labels
     ax[0].set_xlabel('x')
     ax[0].set_ylabel('y')
-    # ax[0].legend()
+    ax[0].legend()
     # ax[0].set_title('State Evolution')
     ax[0].set_title(f't = {frame * tau}')
     
     # Set limit
     ax[0].set_xlim(0, 160)
-    ax[0].set_ylim(0, 10)
+    ax[0].set_ylim(2, 8)
     ax[0].grid(True)
 
     # Plot 
@@ -294,15 +327,30 @@ def update(frame):
     ax[1].plot(time, XL_sol[1, : frame+1], 'ro-', label='vxL')
     
     # # Add labels
-    # ax[1].set_xlabel('x')
-    # ax[1].set_ylabel('u')
-    # # ax[1].legend()
-    # # ax[1].set_title('Control Inputs')
+    ax[1].set_xlabel('t')
+    ax[1].set_ylabel('vx')
+    ax[1].legend()
+    # ax[1].set_title('Control Inputs')
 
     # # Set limit
     ax[1].set_xlim(0, T+2*tau)
-    # ax[1].set_ylim(-0.2, 0.2)
-    # ax[1].grid(True)
+    ax[1].set_ylim(9.5, 15.5)
+    ax[1].grid(True)
+
+    # ax[2]
+    ax[2].plot(time, XF_sol[2, : frame+1], 'bo-', label='axF')
+    ax[2].plot(time, XL_sol[2, : frame+1], 'ro-', label='axL')
+    
+    # # Add labels
+    ax[2].set_xlabel('t')
+    ax[2].set_ylabel('ax')
+    ax[2].legend()
+    # ax[1].set_title('Control Inputs')
+
+    # # Set limit
+    ax[2].set_xlim(0, T+2*tau)
+    ax[2].set_ylim(-2, 2)
+    ax[2].grid(True)
 
     # save fig
     plt.savefig(f'{current_file_path}/log/CMPCGame_{frame}.jpg')
